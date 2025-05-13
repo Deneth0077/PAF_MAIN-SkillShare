a5eef572915f1f65e05ab42ea787cfc42c6bb58b
Deneth0077 <165593633+sachinthana24@users.noreply.github.com>
2025-05-13 18:05:55 +0530

    Edit code in Comment part

--- Frontend/src/components/comments/Comment.tsx
+++ Frontend/src/components/comments/Comment.tsx
@@ -179,7 +179,7 @@ const CommentSection = ({ postId, onCommentCountChange }: CommentSectionProps) =
         </div>
       )}
       
-      {/* Comments list */}
+      {/* Comments list  */}
       {loading ? (
         <div className="flex justify-center py-4">
           <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-indigo-600"></div>
