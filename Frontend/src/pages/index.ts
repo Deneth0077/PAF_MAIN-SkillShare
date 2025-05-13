dea9d3cf68b78ec603925198f91834a7951a5e3c
Hasintha00 <it22595430@my.sliit.lk>
2025-05-13 16:17:53 +0530

    update css and add pages

---
+++ Frontend/src/pages/index.ts
@@ -0,0 +1,5 @@
+export { LearningPlansPage } from './LearningPlansPage';
+export { CreateLearningPlanPage } from './CreateLearningPlanPage';
+export { EditLearningPlanPage } from './EditLearningPlanPage';
+export { LearningPlanDetailPage } from './LearningPlanDetailPage';
+export { TailwindDemoPage } from './TailwindDemoPage'; 
